c104f96ec53a5dc2d69ae7c938cbbee49c1c9bfa
BrandonCuthebrtson <57675300+BrandonCuthebrtson@users.noreply.github.com>
2020-02-17 17:16:34 -0700

    Validation Done

--- Workshop2-TravelExperts/Workshop2-TravelExperts/Forms/EditPackage.cs
+++ Workshop2-TravelExperts/Workshop2-TravelExperts/Forms/EditPackage.cs
@@ -30,6 +30,7 @@ namespace Workshop2_TravelExperts {
         {   //brandons validations
             if (Validator.IsPresent(txtPkgName, "Package Name") == true &&
                 dtpStart.Value < dtpEnd.Value &&
+                dtpStart.Value != dtpEnd.Value &&
                 Validator.IsPresent(txtDesc, "Description") == true &&
                 Validator.IsDecimal(txtBase, "Base Price") == true && Validator.IsNonNegativeDecimal(txtBase, "Base Price") == true &&
                 Validator.IsDecimal(txtAgency, "Agency Commission") == true && Validator.IsNonNegativeDecimal(txtAgency, "Agency Commission") ==true &&
@@ -58,20 +59,7 @@ namespace Workshop2_TravelExperts {
                 {
                     MessageBox.Show(ex.Message, ex.GetType().ToString());
                 }
-                //Brandons Code
-                lblError.Text = "";
-                //Hides error Labels
-                lblErrorPackageName.Visible = true;
-                lblErrorNameMassage.Text = "";
-                lblDateError.Text = "";
-                lblErrorStart.Visible = false;
-                lblErrorEnd.Visible = false;
-                lblDescError.Text = "";
-                lblErrorPackageDesc.Visible = false;
-                lblErrorBasePrice.Visible = false;
-                lblBaseError.Text = "";
-                lblErrorAgencyCommission.Visible = false;
-                lblAgencyError.Text = "";
+               
 
                 Application.Restart();//You have to reload the form when you submit
             }
@@ -88,9 +76,15 @@ namespace Workshop2_TravelExperts {
                     lblErrorNameMassage.Text = "";
                 }
                 
-                if (dtpStart.Value > dtpEnd.Value) //Date Error
+                if (dtpStart.Value > dtpEnd.Value) //Date Error Start is > End
                 {
                     lblDateError.Text = "Start Date must be after End Date";
+                    lblErrorStart.Visible = true;
+                    lblErrorEnd.Visible = true;
+                }
+                else if (dtpStart.Value == dtpEnd.Value)//Date Error Start is = End
+                {
+                    lblDateError.Text = "Start Date cannot be on End Date";
                     lblErrorStart.Visible = true;
                     lblErrorEnd.Visible = true;
                 }
@@ -128,7 +122,7 @@ namespace Workshop2_TravelExperts {
                     lblBaseError.Text = "";
                 }
 
-                if (Validator.IsDecimal(txtAgency, "Agency Commission") == false )
+                if (Validator.IsDecimal(txtAgency, "Agency Commission") == false )//commison error
                 {
                     lblErrorAgencyCommission.Visible = true;
                     lblAgencyError.Text = "Please insert a number that is greater than zero ";
@@ -138,15 +132,18 @@ namespace Workshop2_TravelExperts {
                     lblErrorAgencyCommission.Visible = true;
                     lblAgencyError.Text = "Number inserted must be greater than zero";
                 }
-                else if (Convert.ToDecimal(txtBase.Text) < Convert.ToDecimal(txtAgency.Text))
-                {
-                    lblErrorAgencyCommission.Visible = false;
-                    lblAgencyError.Text = "Agency Commission cannot be greater than the Base price";
-                }
-                else
+                else if (Validator.IsDecimal(txtAgency, "Agency Commission") == true &&
+                    Validator.IsDecimal(txtBase, "Base Price") == true && 
+                    Convert.ToDecimal(txtBase.Text) <= Convert.ToDecimal(txtAgency.Text))
                 {
                     lblErrorAgencyCommission.Visible = true;
                     lblErrorBasePrice.Visible = true;
+                    lblAgencyError.Text = "Agency Commission cannot be greater than or equal to the Base price";
+                }
+                else
+                {
+                    lblErrorAgencyCommission.Visible = false;
+                    
                     lblAgencyError.Text = "";
                 }
             }
