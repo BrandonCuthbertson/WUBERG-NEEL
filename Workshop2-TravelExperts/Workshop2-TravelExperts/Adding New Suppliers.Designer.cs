8fd3b3d74ee7c73c4a90d76c5ff9fd7dca72ddb9
Neel Pandya <neel.pandya@edu.sait.ca>
2020-02-20 10:23:55 -0700

    Changes

--- Workshop2-TravelExperts/Workshop2-TravelExperts/Adding New Suppliers.Designer.cs
+++ Workshop2-TravelExperts/Workshop2-TravelExperts/Adding New Suppliers.Designer.cs
@@ -32,15 +32,13 @@
             this.txtSup = new System.Windows.Forms.TextBox();
             this.btnSubmit = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtSupID = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(114, 117);
+            this.label1.Location = new System.Drawing.Point(127, 64);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(133, 24);
@@ -49,7 +47,7 @@
             // 
             // txtSup
             // 
-            this.txtSup.Location = new System.Drawing.Point(406, 117);
+            this.txtSup.Location = new System.Drawing.Point(406, 56);
             this.txtSup.Margin = new System.Windows.Forms.Padding(4);
             this.txtSup.Name = "txtSup";
             this.txtSup.Size = new System.Drawing.Size(392, 32);
@@ -57,7 +55,7 @@
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(406, 172);
+            this.btnSubmit.Location = new System.Drawing.Point(406, 141);
             this.btnSubmit.Margin = new System.Windows.Forms.Padding(4);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(189, 48);
@@ -68,7 +66,7 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(624, 172);
+            this.btnClear.Location = new System.Drawing.Point(624, 141);
             this.btnClear.Margin = new System.Windows.Forms.Padding(4);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(174, 48);
@@ -77,29 +75,11 @@
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(118, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "SupplierId";
-            // 
-            // txtSupID
-            // 
-            this.txtSupID.Location = new System.Drawing.Point(406, 60);
-            this.txtSupID.Name = "txtSupID";
-            this.txtSupID.Size = new System.Drawing.Size(392, 32);
-            this.txtSupID.TabIndex = 5;
-            // 
             // frmAdding_New_Suppliers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(936, 259);
-            this.Controls.Add(this.txtSupID);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(929, 207);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.txtSup);
@@ -119,7 +99,5 @@
         private System.Windows.Forms.TextBox txtSup;
         private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.Button btnClear;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtSupID;
     }
 }
